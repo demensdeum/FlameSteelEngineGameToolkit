b3cb9ccce4bf02c0f3bcfd8689013d91afa0377e
Demensdeum <demensdeum@gmail.com>
2018-10-21 15:38:50 +0400

    SurfaceMaterial cache

--- Controllers/MaterialLibrary.cpp
+++ Controllers/MaterialLibrary.cpp
@@ -4,6 +4,10 @@ void MaterialLibrary::setMaterialForPath(shared_ptr<FSGLMaterial> material, shar
     pathMaterialMap[*path] = material;
 }
 
+void MaterialLibrary::setSurfaceMaterialForPath(shared_ptr<SurfaceMaterial> surfaceMaterial, shared_ptr<string> path) {
+    pathSurfaceMaterialMap[*path] = surfaceMaterial;
+}
+
 shared_ptr<FSGLMaterial> MaterialLibrary::materialForPath(shared_ptr<string> path) {
 
     auto key = *path;
@@ -16,8 +20,21 @@ shared_ptr<FSGLMaterial> MaterialLibrary::materialForPath(shared_ptr<string> pat
 
 }
 
+shared_ptr<SurfaceMaterial> MaterialLibrary::surfaceMaterialForPath(shared_ptr<string> path) {
+
+    auto key = *path;
+    
+    if (pathSurfaceMaterialMap.find(key) != pathSurfaceMaterialMap.end()) {
+        return pathSurfaceMaterialMap[key];
+    }
+    
+	return shared_ptr<SurfaceMaterial>();
+
+}
+
 void MaterialLibrary::clear() {
     
-    pathMaterialMap.clear();
+	pathSurfaceMaterialMap.clear();
+	pathMaterialMap.clear();
     
 }
--- Controllers/MaterialLibrary.h
+++ Controllers/MaterialLibrary.h
@@ -4,18 +4,24 @@
 #include <map>
 #include <memory>
 #include <FSGL/Data/Material/FSGLMaterial.h>
+#include <FlameSteelFramework/FlameSteelEngineGameToolkit/Data/Components/SurfaceMaterial/SurfaceMaterial.h>
 
 using namespace std;
+using namespace FlameSteelEngine::GameToolkit;
 
 class MaterialLibrary {
 
 public:
 	void setMaterialForPath(shared_ptr<FSGLMaterial> material, shared_ptr<string> path);
+	void setSurfaceMaterialForPath(shared_ptr<SurfaceMaterial> surfaceMaterial, shared_ptr<string> path);
+
 	shared_ptr<FSGLMaterial> materialForPath(shared_ptr<string> path);
+	shared_ptr<SurfaceMaterial> surfaceMaterialForPath(shared_ptr<string> path);
 	void clear();
 
 private:
 	map<string, shared_ptr<FSGLMaterial>> pathMaterialMap;
+	map<string, shared_ptr<SurfaceMaterial>> pathSurfaceMaterialMap;
 
 };
 
